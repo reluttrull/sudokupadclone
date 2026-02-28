c99f593c0bce2fc79c00330fc158d8e9d30e1de3
Ryan Luttrull <reluttrull@gmail.com>
2026-02-28 13:42:02 -0600

    Adding more controls

--- sudokupadclone/src/puzzle/components/Controls.tsx
+++ sudokupadclone/src/puzzle/components/Controls.tsx
@@ -1,5 +1,4 @@
-import { RiNumber9 } from 'react-icons/ri'
-import { TbNumber123 } from 'react-icons/tb'
+import { RiNumber1, RiNumber2, RiNumber3, RiNumber4, RiNumber5, RiNumber6, RiNumber7, RiNumber8, RiNumber9, RiNumber0 } from 'react-icons/ri'
 import { InputType } from '../enums'
 
 interface ControlsProps {
@@ -9,12 +8,50 @@ interface ControlsProps {
 function Controls({ activeInputType, onInputTypeChanged }: ControlsProps) {
     return (
         <>
-            <button className={activeInputType == InputType.BigNumber ? 'active-button' : 'inactive-button'} onClick={() => onInputTypeChanged(InputType.BigNumber)} >
-                <RiNumber9 />
-            </button >
-            <button className={activeInputType == InputType.SmallCenterNumber ? 'active-button' : 'inactive-button'} onClick={() => onInputTypeChanged(InputType.SmallCenterNumber)}>
-                <TbNumber123 />
-            </button>
+            <div className="controls-container">
+                <div className="number-pad controls-item">
+                    <button className="number-key square-button-icon">
+                        <RiNumber1 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber2 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber3 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber4 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber5 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber6 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber7 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber8 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber9 />
+                    </button >
+                    <button className="number-key square-button-icon">
+                        <RiNumber0 />
+                    </button >
+                </div>
+                <div className="controls-item">
+                    <div><button className={activeInputType == InputType.BigNumber ? 'active-button square-button-icon' : 'inactive-button square-button-icon'}
+                                onClick={() => onInputTypeChanged(InputType.BigNumber)} >
+                        <img src="/bigNumberInput.png" />
+                    </button></div>
+                    <div><button className={activeInputType == InputType.SmallCenterNumber ? 'active-button square-button-icon' : 'inactive-button square-button-icon'}
+                                onClick={() => onInputTypeChanged(InputType.SmallCenterNumber)}>
+                        <img src="/smallCenterNumbersInput.png" />
+                    </button></div>
+                </div>
+            </div>
         </>
     )
 }

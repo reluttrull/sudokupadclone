81fafd201e5d399bba662da84e283c3ea9f5fcb8
Ryan Luttrull <reluttrull@gmail.com>
2026-03-09 09:07:01 -0500

    Update base path and workflow

--- sudokupadclone/vite.config.ts
+++ sudokupadclone/vite.config.ts
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/sudokupadclone/',
     plugins: [plugin()],
     server: {
         port: 52080,
